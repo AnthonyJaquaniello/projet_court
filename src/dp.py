638d18c894ff0eebffa92059339efc0e51a1a39b
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-09-14 12:22:21 +0200

    supplement interface shell, new calcul pour dist_to_dope, resolution bug, paquetage

--- src/dp.py
+++ src/dp.py
@@ -1,8 +1,8 @@
 import sys
 from math import sqrt
 from math import floor
-import numpy as np
 import re
+import numpy as np
 
 ## \mainpage Projet court de M2 Bioinformatique
 # \section intro_sec Introduction
@@ -12,11 +12,15 @@ import re
 #compatibles avec une structure. Seul les carbones α de la protéine seront
 #considérés. Vous utiliserez les potentiels statistiques DOPE [5].
 # \section ressources Ressources
-# 1) Jones, D.T., Taylor, W.R. & Thornton, J.M. (1992) A new approach to protein fold recognition. Nature. 358, 86-89.\n
-# 2) Jones, D.T., Miller, R.T. & Thornton, J.M. (1995) Successful protein fold recognition by optimal sequence threading validated by
+# 1) Jones, D.T., Taylor, W.R. & Thornton, J.M. (1992)
+#A new approach to protein fold recognition. Nature. 358, 86-89.\n
+# 2) Jones, D.T., Miller, R.T. & Thornton, J.M. (1995)
+#Successful protein fold recognition by optimal sequence threading validated by
 #rigorous blind testing. Proteins. 23, 387-397.\n
-# 3) Jones, D.T. (1998) THREADER : Protein Sequence Threading by Double Dynamic Programming. (in) Computational Methods in
-#Molecular Biology. Steven Salzberg, David Searls, and Simon Kasif, Eds. Elsevier Science. Chapter 13.\n
+# 3) Jones, D.T. (1998) THREADER : Protein Sequence Threading by
+#Double Dynamic Programming. (in) Computational Methods in
+#Molecular Biology. Steven Salzberg, David Searls, and Simon
+#Kasif, Eds. Elsevier Science. Chapter 13.\n
 # 4) Protein Structure Comparison Using SAP - Springer\n
 # 5) http://www.dsimb.inserm.fr/~gelly/doc/dope.par
 
@@ -80,81 +84,96 @@ def dope_parser(dope):
 
 def dist_to_dope(dist, name_1, name_2, liste):
     """Fais la conversion entre une distance entre deux résidus, et un
-     potentiel DOPE dans la structure de donnée 'liste'.
+    potentiel DOPE dans la structure de donnée 'liste'.
     Nécessite le nom des deux résidus dont la distance a été évaluée."""
     for d in liste:
         if d["AA_1"] == name_1:
             if d["AA_2"] == name_2:
                 if dist != 0:
-                    return float(d["Energy"][floor(dist*4) - 1])
-            return float(d["Energy"][0])
+                    return (float(d["Energy"][floor(dist*4) - 1]) + float(d["Energy"][floor(dist*4)])) / 2
+                return 0
 
 def min_finder(matrix):
     """Renvoie le minimum d'une matrice."""
     liste = list(matrix.reshape(matrix.size))
     return min(liste)
-#==================================================
+if __name__ == '__main__':
 
-#Dans un premier temps il s'agit de calculer une matrice de distance,
-#entre tous les acides aminés de la protéine, deux à deux.
-NB_AA = 50
-NB_CA = 50
-PATH_PDB = sys.argv[1]
-PATH_FASTA = sys.argv[2]
-IN_DOPE = sys.argv[3]
-OUT_DOPE = sys.argv[4]
-print("Veuillez patienter, le calcul peut prendre un certain temps...")
+    #Interface utilisateur
 
-liste_ca = pdb_parser(PATH_PDB)
-seq = fasta_parser(PATH_FASTA)
-dist_matrix = np.zeros((NB_AA, NB_CA))
+    NB_AA = 35
+    NB_CA = 35
+    try:
+        PATH_PDB = sys.argv[1]
+        PATH_FASTA = sys.argv[2]
+        IN_DOPE = sys.argv[3]
+        OUT_DOPE = sys.argv[4]
+        print("Prenez-vous un café, le calcul peut prendre un certain temps :p")
+    except IndexError:
+        sys.exit("Non non non, il faut 4 arguments en tout, matez le README (è_é)")
+    liste_ca = pdb_parser(PATH_PDB)
+    seq = fasta_parser(PATH_FASTA)
+    dist_matrix = np.zeros((NB_AA, NB_CA))
 
-for i in range(0, dist_matrix.shape[0]):
-    for j in range(0, dist_matrix.shape[1]):
-        dist_matrix[i, j] = liste_ca[i].euclidean_dist(liste_ca[j])
-#Dans un second temps on va devoir relier l'information de distance avec le potentiel DOPE
-catch_number = re.compile("-{0,}[0-9]{1,}.[0-9]{1,}")
-catch_name = re.compile("[A-Z]{1,3}")
-dope_limitator(IN_DOPE, OUT_DOPE)
-liste_dope = dope_parser(OUT_DOPE)
-abreviation = {
-    "A": "ALA", "C": "CYS", "D": "ASP", "E": "GLU", "F": "PHE",
-    "G": "GLY", "H": "HIS", "I": "ILE", "L": "LEU", "K": "LYS", "M": "MET",
-    "N": "ASN", "P": "PRO", "Q": "GLN", "R": "ARG", "S": "SER", "T": "THR",
-    "V": "VAL", "W": "TRP", "Y": "TYR"
-}
+    #Dans un premier temps il s'agit de calculer une matrice de distance,
+    #entre tous les acides aminés de la protéine, deux à deux.
 
-prime_matrix = np.zeros((NB_AA, NB_CA, dist_matrix.shape[0] + 1, dist_matrix.shape[1] + 1))
-#on stocke dans chaque case de cette matrice, une low_matrix
+    for i in range(0, dist_matrix.shape[0]):
+        for j in range(0, dist_matrix.shape[1]):
+            dist_matrix[i, j] = liste_ca[i].euclidean_dist(liste_ca[j])
 
-#construction des low_lvl_matrix:
+    #Dans un second temps on va devoir relier l'information de distance
+    #avec un potentiel DOPE dans le fichier.dope donné en entrée.
 
-for i in range(0, NB_AA): #résidu
-    for j in range(0, NB_CA): #ca
-        #pour chaque couple on doit créer une low level matrix
-        low_level_matrix = np.zeros((NB_AA + 1, NB_CA + 1))
-        for k in range(1, low_level_matrix.shape[0]):
-            for l in range(1, low_level_matrix.shape[1]):
-                choice = min(low_level_matrix[k-1, l-1], low_level_matrix[k-1, l],
-                             low_level_matrix[k, l-1])
-                distance = liste_ca[j].euclidean_dist(liste_ca[l-1])
-                try:
-                    energy = dist_to_dope(distance, abreviation[seq[i]],
-                                          abreviation[seq[k-1]], liste_dope)
-                except IndexError:
-                    energy = 0
-                low_level_matrix[k, l] = choice + energy
-        prime_matrix[i, j, :, :] = low_level_matrix
+    catch_number = re.compile("-{0,}[0-9]{1,}.[0-9]{1,}")
+    catch_name = re.compile("[A-Z]{1,3}")
+    dope_limitator(IN_DOPE, OUT_DOPE)
+    liste_dope = dope_parser(OUT_DOPE)
+    abreviation = {
+        "A": "ALA", "C": "CYS", "D": "ASP", "E": "GLU", "F": "PHE",
+        "G": "GLY", "H": "HIS", "I": "ILE", "L": "LEU", "K": "LYS", "M": "MET",
+        "N": "ASN", "P": "PRO", "Q": "GLN", "R": "ARG", "S": "SER", "T": "THR",
+        "V": "VAL", "W": "TRP", "Y": "TYR"
+    }
 
-#remplissage de la high_lvl_matrix:
+    prime_matrix = np.zeros((NB_AA, NB_CA, dist_matrix.shape[0] + 1, dist_matrix.shape[1] + 1))
+    #On stocke dans chaque case de cette matrice, une low_level_matrix.
+    #Construction des low_lvl_matrix selon un algorithme de Needleman&Wunsch:
 
-high_level_matrix = np.zeros((NB_AA + 1, NB_CA + 1))
+    for i in range(0, NB_AA): #résidu
+        for j in range(0, NB_CA): #ca
+            #pour chaque couple on doit créer une low level matrix
+            low_level_matrix = np.zeros((NB_AA + 1, NB_CA + 1))
+            for k in range(1, low_level_matrix.shape[0]):
+                for l in range(1, low_level_matrix.shape[1]):
+                    #3 choix possible pour arriver a la case (k,l),
+                    #on choisit celle avec le score le plus bas.
+                    choice = min(low_level_matrix[k-1, l-1], low_level_matrix[k-1, l],
+                                 low_level_matrix[k, l-1])
+                    #On récupère la distance entre le carbone alpha fixé et l'actuel.
+                    distance = dist_matrix[j, l-1]
+                    try:
+                        #Conversion en potentiel énergétique.
+                        energy = dist_to_dope(distance, abreviation[seq[i]],
+                                              abreviation[seq[k-1]], liste_dope)
+                    except IndexError:
+                        #Si la distance entre 2 carbones alphas est trop grande,
+                        #l'énergie d'interaction est négligeable.
+                        energy = 0
+                    low_level_matrix[k, l] = choice + energy
+            prime_matrix[i, j, :, :] = low_level_matrix
 
-for i in range(1, high_level_matrix.shape[0]):
-    for j in range(1, high_level_matrix.shape[1]):
-        choice = min([high_level_matrix[i-1, j-1], high_level_matrix[i-1, j],
-                      high_level_matrix[i, j-1]])
-        high_level_matrix[i, j] = choice + min_finder(prime_matrix[i-1, j-1, :, :])
-#resultat final:
-print("Score d'adéquation entre la séquence '{}' et la structure '{}' = {:.2f}"
-      .format(PATH_FASTA, PATH_PDB, min_finder(high_level_matrix)))
+    #Remplissage de la high_lvl_matrix:
+
+    high_level_matrix = np.zeros((NB_AA + 1, NB_CA + 1))
+
+    for i in range(1, high_level_matrix.shape[0]):
+        for j in range(1, high_level_matrix.shape[1]):
+            choice = min([high_level_matrix[i-1, j-1], high_level_matrix[i-1, j],
+                          high_level_matrix[i, j-1]])
+            #C'est le minimum de la Low_level_matrix correspondante qui est injecté
+            #dans la case de la High level matrix.
+            high_level_matrix[i, j] = choice + min_finder(prime_matrix[i-1, j-1, :, :])
+    #Resultat final:
+    print("Score d'adéquation entre la séquence '{}' et la structure '{}' = {:.2f}"
+          .format(PATH_FASTA, PATH_PDB, min_finder(high_level_matrix)))
